ecdea24923274a318f7af4d211157517a81b1cdf
Tobias Lauritzen <TobiasLauritzen@protonmail.com>
2022-07-20 12:41:21 +0200

    readded medarbejder

--- AccessToJson/Program.cs
+++ AccessToJson/Program.cs
@@ -6,43 +6,59 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 var configurationRoot = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("./appsettings.json").Build();
+  .AddJsonFile("./appsettings.json")
+  .Build();
+
 var databasePath = configurationRoot["DatabasePath"];
-// var medarbejderDatabasePath = configurationRoot["MedarbejderDatabasePath"];
+var medarbejderDatabasePath = configurationRoot["MedarbejderDatabasePath"];
 var outputPath = configurationRoot["OutputPath"];
+
+ArgumentNullException.ThrowIfNull(databasePath, "databasePath is not set in appSettings");
+ArgumentNullException.ThrowIfNull(medarbejderDatabasePath, "medarbejderDatabasePath is not set in appSettings");
+ArgumentNullException.ThrowIfNull(outputPath, "outputPath is not set in appSettings");
+
 new DirectoryInfo(outputPath).Create();
 
 await using var con = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={databasePath};");
-await con.OpenAsync();
-// await using var medCon = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={medarbejderDatabasePath};");
-// await medCon.OpenAsync();
+await using var medCon
+  = new OleDbConnection($"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={medarbejderDatabasePath};");
+
+var conOpenTask = con.OpenAsync();
+var medConOpenTask = medCon.OpenAsync();
+await conOpenTask;
+await medConOpenTask;
 
 var tasks = new List<Task>
 {
-    ReadModelToFile<TabBatch>(con),
-    ReadModelToFile<TabFejl>(con),
-    ReadModelToFile<TabRulle>(con),
-    ReadModelToFile<TblLevering>(con),
-    // ReadModelToFile<TabMedarbejder>(medCon)
+  ReadModelToFile<TabBatch>(con),
+  ReadModelToFile<TabFejl>(con),
+  ReadModelToFile<TabRulle>(con),
+  ReadModelToFile<TblLevering>(con),
+  ReadModelToFile<TabMedarbejder>(medCon)
 };
 await Task.WhenAll(tasks);
-await con.CloseAsync();
+
+var conCloseTask = con.CloseAsync();
+var medConCloseTask = medCon.CloseAsync();
+await conCloseTask;
+await medConCloseTask;
 
 Console.WriteLine("All done!");
 Environment.Exit(0);
 
 async Task ReadModelToFile<T>(IDbConnection oleDbConnection)
 {
-    Console.WriteLine($"Exporting {typeof(T).Name}");
-    var data = (await oleDbConnection.QueryAsync<T>($"select * from {typeof(T).Name}")).ToList();
-    Console.WriteLine($"Found {data.Count} rows in {typeof(T).Name}");
+  var dataTask = oleDbConnection.QueryAsync<T>($"select * from {typeof(T).Name}");
+  
+  Console.WriteLine($"Exporting {typeof(T).Name}");
+  var data = (await dataTask).ToList();
+  Console.WriteLine($"Found {data.Count} rows in {typeof(T).Name}");
 
-    var filePath = Path.Combine(outputPath, $"{typeof(T).Name}.json");
-    await using var file = File.CreateText(filePath);
-    var serializer = new JsonSerializer
-    {
-        DateFormatString = "dd-MM-yyyy HH:mm:ss",
-        Converters = { new StringJsonConverter() }
-    };
-    serializer.Serialize(file, new Dictionary<string, List<T>> { { typeof(T).Name, data } });
+  var filePath = Path.Combine(outputPath, $"{typeof(T).Name}.json");
+  await using var file = File.CreateText(filePath);
+  var serializer = new JsonSerializer
+  {
+    DateFormatString = "dd-MM-yyyy HH:mm:ss", Converters = { new StringJsonConverter() }
+  };
+  serializer.Serialize(file, new Dictionary<string, List<T>> { { typeof(T).Name, data } });
 }
